b159fb5c018d1942d0f0b3fa5186a17da1dbf601
Jorge <sejidjorge@gmail.com>
2021-12-10 17:48:45 -0300

    nav bar finalizada

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ import SettingsIcon from '@mui/icons-material/Settings';
 
 function App() {
   return (
-    <div className="App bg-gray-800 w-screen h-screen justify-around flex">
-      <div className="bg-gradient-to-r from-teal-600 via-purple-500 to-violet-600 w-screen h-16 flex justify-around p-4">
-        <div className="w-6/12 flex space-x-8">
-          <a className="text-white items-center flex hover:text-stone-300" href="#"><HomeIcon className="mr-2"/> INICIO</a>
-          <a className="text-white items-center flex hover:text-stone-300" href="#"><PodcastsIcon className="mr-2"/> PODCASTS</a>
-          <a className="text-white items-center flex hover:text-stone-300" href="#"><HeadphonesIcon className="mr-2"/> BIBLIOTECA</a>
+    <div className="App bg-gray-800 w-screen h-screen flex">
+      <div className="bg-gradient-to-r from-teal-600 via-purple-500 to-violet-600 w-screen h-16 items-center flex justify-between p-4">
+        <div className="w-6/12 ml-32 flex space-x-8">
+          <a className="text-white items-center hover:text-stone-300" href="#"><HomeIcon className="mr-2"/> INICIO</a>
+          <a className="text-white items-center hover:text-stone-300" href="#"><PodcastsIcon className="mr-2"/> PODCASTS</a>
+          <a className="text-white items-center hover:text-stone-300" href="#"><HeadphonesIcon className="mr-2"/> BIBLIOTECA</a>
         </div>
         <div className="w-4/12 flex space-x-6">
           <form>
